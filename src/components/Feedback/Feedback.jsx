71134ff25e654f1d784de2713b99f6feb8cabb57
alionachykal <106018567+alionachykal@users.noreply.github.com>
2022-12-21 18:45:36 +0300

    Feedback homework

---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,23 @@
+import  propTypes  from 'prop-types';
+import css from './Feedback.module.css';
+
+
+   
+export const Feedback = ({ options, onLeaveFeedback }) => (
+  <div>
+    {options.map((option, index) => (
+      <button
+        key={index}
+        onClick={() => onLeaveFeedback(option)}
+        className={css.feedbackBtn}
+      >
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+Feedback.propTypes = {
+  options: propTypes.arrayOf(propTypes.string).isRequired,
+  onLeaveFeedback: propTypes.func.isRequired,
+};
