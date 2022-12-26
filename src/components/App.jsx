1d3c4911cc5406ef669d6eddbb7b07fefd2c9b85
alionachykal <106018567+alionachykal@users.noreply.github.com>
2022-12-27 01:49:01 +0300

    fixed mistakes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,16 +16,13 @@ export class App extends Component {
     bad: 0,
   };
 
-  handleFeedback = e => {
-    if (e === 'Good') {
-      this.setState({ good: this.state.good + 1 });
-    } else if (e === 'Neutral') {
-      this.setState({ neutral: this.state.neutral + 1 });
-    } else if (e === 'Bad') {
-      this.setState({ bad: this.state.bad + 1 });
-    }
-  };
-
+  onLeaveFeedback = e => {
+ const {name} = e.target;
+  this.setState(prevState => ({
+  [name]:prevState[name] + 1
+ }))
+}
+ 
   totalFeedback = () => {
     let total = this.state.good + this.state.neutral + this.state.bad;
     return total;
@@ -37,8 +34,9 @@ export class App extends Component {
     }
     return Math.round((this.state.good / this.totalFeedback()) * 100);
   };
-
+    
   render() {
+     const { good, neutral, bad } = this.state;
     return (
       <div
         style={{
@@ -52,17 +50,18 @@ export class App extends Component {
       >
         <Section title="Please leave feedback">
           <Feedback
-            options={['Good', 'Neutral', 'Bad']}
-            onLeaveFeedback={this.handleFeedback}
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.onLeaveFeedback}
           />{' '}
         </Section>
 
         <Section title="Statistics">
           {this.totalFeedback() !== 0 ? (
+          
             <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
+              good={good}
+              neutral={neutral}
+              bad={bad}
               total={this.totalFeedback()}
               positivePercentage={this.positivePercentage()}
             />
